67011d395ec6ef6986d94b4ef742ffd0af7f9e04
Christer Enfors <christer.enfors@gmail.com>
2014-07-11 01:01:30 +0200

    Add some initial code.

--- lunar-lander.lisp
+++ lunar-lander.lisp
@@ -1,1 +1,18 @@
 ;;;; Lunar lander - a learning example by Christer Enfors
+
+(defclass lander ()
+  ((altitude :initarg :altitude)
+   (speed    :initarg :speed)
+   (fuel     :initarg :fuel)))
+
+(defgeneric update (lander fuel)
+  (:documentation "Update the lander's speed and altitude."))
+
+(defmethod update ((lander lander) fuel)
+  (print "Updating lander."))
+
+(defmethod print-object ((lander lander) stream)
+  (format stream "A lander!~%Altitude: ~a~%Speed: ~a~%Fuel: ~a~%"
+	  (slot-value lander 'altitude)
+	  (slot-value lander 'speed)
+	  (slot-value lander 'fuel)))
